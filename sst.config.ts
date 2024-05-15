0656514f2e5a75b82f8bd34af8ef9ba515217491
Jhon Escobar <jescobar@nestsystems.com>
2024-05-15 06:17:09 -0700

    Add userGameHistory and leaderboard table

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,6 @@
 import { SSTConfig } from "sst";
 
-import {
-  CICD,
-  FirehoseStack,
-  FrontendDistribution,
-  S3Origin,
-  WebACL,
-  ApiStack,
-  userApiStack,
-} from "@app/backend/stacks";
+import { CICD, FirehoseStack, FrontendDistribution, S3Origin, WebACL, ApiStack, userApiStack, Database } from "@app/backend/stacks";
 
 import { SST_APP_NAME } from "@app/backend/libs/config";
 
@@ -27,6 +19,7 @@ export default {
 
     app
       /* Backend */
+      .stack(Database)
       .stack(ApiStack)
       .stack(userApiStack)
       /* Frontend */
