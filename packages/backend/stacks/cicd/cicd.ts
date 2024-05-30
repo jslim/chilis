5c02e0a8a148dc92d35272781fcb1731d429d323
Donghyuk Jang <donghyuk0117@gmail.com>
2024-05-30 15:31:26 -0400

    fix: update cicd for app admin

--- packages/backend/stacks/cicd/cicd.ts
+++ packages/backend/stacks/cicd/cicd.ts
@@ -156,6 +156,8 @@ export function CICD({ stack, app }: StackContext) {
             commands: [
               'echo "Using Node.js version $(node -v)"',
               "echo Build started",
+              'NEXT_PUBLIC_VERSION_NUMBER=$(echo "$CODEBUILD_BUILD_NUMBER")',
+              'NEXT_PUBLIC_COMMIT_ID=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c1-6)',
               "npm run deploy",
               // Add your build commands here
               "echo Build completed",
