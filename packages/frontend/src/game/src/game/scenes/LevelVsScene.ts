be1ff295ce8b608f3b6e203d8d13c0a519c48ced
Mark <mark@mediamonks.com>
2024-05-31 18:52:14 +0200

    Remove skip button from vs video

--- packages/frontend/src/game/src/game/scenes/LevelVsScene.ts
+++ packages/frontend/src/game/src/game/scenes/LevelVsScene.ts
@@ -1,5 +1,4 @@
 import { Scene } from './Scene'
-import { GAME_HEIGHT, GAME_WIDTH } from '@/game/src/game/game.config'
 import type SceneManager from '@/game/src/game/scenes/SceneManager'
 
 export class LevelVsScene extends Scene {
@@ -19,7 +18,6 @@ export class LevelVsScene extends Scene {
     ;(async () => {
       let levelNo = 1 //this.levelNo
       await this.playVideo(`vs_level_0${levelNo}`, () => gotoLevel())
-      this.addButton('SKIP', [GAME_WIDTH - 40, GAME_HEIGHT - 20], () => gotoLevel())
     })()
   }
 }
