2ac6c71ab0e9b924d6808239cd74fc018ae8e7d2
Jose Delgado <josdelgaur@gmail.com>
2024-06-03 17:34:23 -0300

    feature: 404 page

--- packages/frontend/src/components/PageNotFound/PageNotFound.view.tsx
+++ packages/frontend/src/components/PageNotFound/PageNotFound.view.tsx
@@ -2,15 +2,14 @@ import type { FC } from 'react'
 import type { PageHandle } from '@/data/types'
 import type { ControllerProps } from './PageNotFound.controller'
 
-import { useEffect, useImperativeHandle } from 'react'
 import classNames from 'classnames'
-import { gsap } from 'gsap'
 
 import css from './PageNotFound.module.scss'
 
 import { copy } from '@/utils/copy'
 
 import { useRefs } from '@/hooks/use-refs'
+import SvgChilis from '@/svgs/Chilis.svg'
 
 export interface ViewProps extends ControllerProps {}
 
@@ -23,18 +22,11 @@ export type ViewRefs = {
 export const View: FC<ViewProps> = ({ content, onReady }) => {
   const refs = useRefs<ViewRefs>()
 
-  useEffect(() => {
-    onReady?.(refs.pageHandle)
-    gsap.set(refs.root.current, { opacity: 0 })
-  }, [refs, onReady])
-
-  useImperativeHandle(refs.pageHandle, () => ({
-    animateOut: () => gsap.timeline().to(refs.root.current, { opacity: 0 }),
-    animateIn: () => gsap.timeline().to(refs.root.current, { opacity: 1 })
-  }))
-
   return (
     <main className={classNames('PageNotFound', css.root)} ref={refs.root}>
+      <div className={css.logoContainer}>
+        <SvgChilis />
+      </div>
       <h1 className={css.title} {...copy.html(content.body.title)} />
     </main>
   )
