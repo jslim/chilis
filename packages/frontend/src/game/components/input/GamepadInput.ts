edd55c60453783b3a4b9554019a08a3c717ae9b5
Mark <mark@mediamonks.com>
2024-06-07 16:57:24 +0200

    Make gamepad more flexible, use axis controls and all action buttons

--- packages/frontend/src/game/components/input/GamepadInput.ts
+++ packages/frontend/src/game/components/input/GamepadInput.ts
@@ -4,8 +4,8 @@ import { Input } from './Input'
 export class GamepadInput extends Component {
   override onStart() {
     super.onStart()
-    window.addEventListener('gamepadconnected', this.onGamepadConnected.bind(this))
-    window.addEventListener('gamepaddisconnected', this.onGamepadDisconnected.bind(this))
+    window.addEventListener('gamepadconnected', this.onGamepadConnected)
+    window.addEventListener('gamepaddisconnected', this.onGamepadDisconnected)
   }
 
   onGamepadConnected(event: GamepadEvent) {
@@ -32,16 +32,37 @@ export class GamepadInput extends Component {
     if (!input) return
 
     // D-Pad buttons are usually mapped to buttons[12] (up), buttons[13] (down), buttons[14] (left), buttons[15] (right)
-    const upPressed = gamepad.buttons[12]?.pressed ?? false
-    const downPressed = gamepad.buttons[13]?.pressed ?? false
-    const leftPressed = gamepad.buttons[14]?.pressed ?? false
-    const rightPressed = gamepad.buttons[15]?.pressed ?? false
+    let upPressed = gamepad.buttons[12]?.pressed ?? false
+    let downPressed = gamepad.buttons[13]?.pressed ?? false
+    let leftPressed = gamepad.buttons[14]?.pressed ?? false
+    let rightPressed = gamepad.buttons[15]?.pressed ?? false
+    const leftStickX = gamepad.axes[0]
+    const leftStickY = gamepad.axes[1]
+    // map angle to upPressed/downPressed/leftPressed/rightPressed
+    const angle = Math.atan2(leftStickY, leftStickX)
+    // get direction from angle
+    if (Math.abs(angle) < Math.PI / 4) {
+      if (Math.abs(gamepad.axes[0]) > 0.1) rightPressed = true
+    } else if (Math.abs(angle) > (3 * Math.PI) / 4) {
+      if (Math.abs(gamepad.axes[0]) > 0.1) leftPressed = true
+    } else if (angle > 0) {
+      if (Math.abs(gamepad.axes[1]) > 0.1) downPressed = true
+    } else if (Math.abs(gamepad.axes[1]) > 0.1) upPressed = true
+
+    // action button
+    const actionPressed =
+      gamepad.buttons[4]?.pressed ||
+      gamepad.buttons[5]?.pressed ||
+      gamepad.buttons[6]?.pressed ||
+      gamepad.buttons[7]?.pressed ||
+      false
 
     // directly write in map
     input.map.set('up', upPressed)
     input.map.set('down', downPressed)
     input.map.set('left', leftPressed)
     input.map.set('right', rightPressed)
+    input.map.set('action', actionPressed)
   }
 
   override destroy() {
