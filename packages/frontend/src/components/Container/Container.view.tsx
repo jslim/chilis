664648ae89c528fefbf7c1962486f762f5773f11
Jose Delgado <josdelgaur@gmail.com>
2024-06-09 19:29:37 -0300

    feature: frame border

--- packages/frontend/src/components/Container/Container.view.tsx
+++ packages/frontend/src/components/Container/Container.view.tsx
@@ -1,6 +1,6 @@
 import type { ControllerProps } from './Container.controller'
 
-import { type FC, useEffect } from 'react'
+import { type FC, useEffect, useState } from 'react'
 import classNames from 'classnames'
 
 import css from './Container.module.scss'
@@ -18,15 +18,26 @@ export type ViewRefs = {
 // View (pure and testable component, receives props exclusively from the controller)
 export const View: FC<ViewProps> = ({ className }) => {
   const refs = useRefs<ViewRefs>()
+  const [showGameBorder, setShowGameBorder] = useState<boolean>(false)
 
   useEffect(() => {
-    initializeGame()
+    const gameInstance = initializeGame()
+
+    if (gameInstance) {
+      const handleShowGameBorder = (showBorder: boolean) => {
+        setShowGameBorder(showBorder)
+      }
+
+      gameInstance.then((game) => {
+        game.onShowGameBorder.subscribe(handleShowGameBorder)
+      })
+    }
   }, [])
 
   return (
     <div className={classNames('Container', css.root, className)} ref={refs.root}>
       {/* Game Container */}
-      <div id="app" />
+      <div id="app" className={classNames(css.game, { [css.hasBorder]: showGameBorder })} />
     </div>
   )
 }
