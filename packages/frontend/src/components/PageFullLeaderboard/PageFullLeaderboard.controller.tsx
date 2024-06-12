ca321a756399bcf1b98a275cf5d7faf22c38d04b
Alejandro Mesa <notalemesa@gmail.com>
2024-06-12 11:25:11 -0400

    fix: full leaderboard

--- packages/frontend/src/components/PageFullLeaderboard/PageFullLeaderboard.controller.tsx
+++ packages/frontend/src/components/PageFullLeaderboard/PageFullLeaderboard.controller.tsx
@@ -1,7 +1,9 @@
 import type { FC } from 'react'
 import type { ApiResponse, PageProps } from '@/data/types'
 
-import { memo, useEffect, useState } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
+
+import { localState } from '@/store'
 
 import { Endpoints, fetchApi } from '@/utils/fetchApi'
 
@@ -14,40 +16,43 @@ export const Controller: FC<ControllerProps> = memo((props) => {
   const [arrayOfPlayers, setArrayOfPlayers] = useState<ApiResponse['data']>([])
   //  const [loading, setLoading] = useState<boolean>(true) TODO: in case we need to show a loading spinner
   // const [error, setError] = useState<string | null>(null) TODO: in case we need to show an error message
+  const userToken = localState().user.accessToken
 
-  const handleApiCall = async () => {
+  const handleApiCall = useCallback(async () => {
     try {
       const response = await fetchApi(`${process.env.NEXT_PUBLIC_API_URL + Endpoints.LEADERBOARD}`, '100', {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userToken}`
         }
       })
 
       const apiResponse = response as ApiResponse
 
-      if (Array.isArray(apiResponse) && apiResponse.length > 0) {
-        console.log('Success:', apiResponse)
-        setArrayOfPlayers(apiResponse)
-        return apiResponse
+      if (Array.isArray(apiResponse.leaderboard)) {
+        if (apiResponse.leaderboard.length > 0) {
+          return apiResponse
+        }
+        console.error('Leaderboard is empty:', apiResponse)
+      } else {
+        console.error('An error occurred:', apiResponse)
       }
-
-      console.error('An error ocurred:', apiResponse.message)
     } catch (error_) {
       console.error(error_)
     }
-  }
+  }, [userToken])
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await handleApiCall()
-      if (response && Array.isArray(response.data)) {
-        setArrayOfPlayers(response.data)
+      if (response && Array.isArray(response.leaderboard) && response.leaderboard.length > 0) {
+        setArrayOfPlayers(response.leaderboard)
       }
     }
 
     fetchData()
-  }, [])
+  }, [handleApiCall])
 
   return <View {...props} arrayOfPlayers={arrayOfPlayers} />
 })
