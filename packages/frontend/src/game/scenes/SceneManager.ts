7f12c41e3459256aa74094131b222ac50991dbb2
Mark <mark@mediamonks.com>
2024-06-12 16:22:01 +0200

    Emit game over after finishing last level

--- packages/frontend/src/game/scenes/SceneManager.ts
+++ packages/frontend/src/game/scenes/SceneManager.ts
@@ -60,7 +60,11 @@ export default class SceneManager {
   levelComplete(result: GameStateValues) {
     this.gameController.onLevelComplete.emit(result)
     const nextLevelNo = result.level + 1
-    this.showLevelIntro(nextLevelNo)
+    if (nextLevelNo <= 18) {
+      this.showLevelIntro(nextLevelNo)
+    } else {
+      this.gameController.onGameOver.emit(result)
+    }
   }
 
   end(result: GameStateValues) {
