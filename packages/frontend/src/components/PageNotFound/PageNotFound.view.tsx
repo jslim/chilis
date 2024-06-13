651b2084b66c6f526303937cda263625809759a3
Alejandro Mesa <notalemesa@gmail.com>
2024-06-13 12:15:02 -0400

    chore: 404

--- packages/frontend/src/components/PageNotFound/PageNotFound.view.tsx
+++ packages/frontend/src/components/PageNotFound/PageNotFound.view.tsx
@@ -1,16 +1,22 @@
-import type { FC } from 'react'
 import type { PageHandle } from '@/data/types'
 import type { ControllerProps } from './PageNotFound.controller'
 
+import { type FC, useEffect } from 'react'
 import classNames from 'classnames'
+import gsap from 'gsap'
 
 import css from './PageNotFound.module.scss'
+
+import { routes } from '@/data/routes'
 
 import { copy } from '@/utils/copy'
 
 import { useRefs } from '@/hooks/use-refs'
 
+import { BaseButton } from '@/components/BaseButton'
+
 import SvgChilis from '@/svgs/Chilis.svg'
+import Reboot from '@/svgs/Reboot.svg'
 
 export interface ViewProps extends ControllerProps {}
 
@@ -23,12 +29,23 @@ export type ViewRefs = {
 export const View: FC<ViewProps> = ({ content }) => {
   const refs = useRefs<ViewRefs>()
 
+  useEffect(() => {
+    gsap.timeline().to(refs.root.current, { opacity: 1 })
+  }, [refs])
+
   return (
     <main className={classNames('PageNotFound', css.root)} ref={refs.root}>
       <div className={css.logoContainer}>
         <SvgChilis />
       </div>
-      <h1 className={css.title} {...copy.html(content.body.title)} />
+
+      <div className={css.container}>
+        <h1 className={css.title} {...copy.html(content.body.title)} />
+
+        <BaseButton className={css.button} href={routes.HOME}>
+          <Reboot className={css.svg} />
+        </BaseButton>
+      </div>
     </main>
   )
 }
