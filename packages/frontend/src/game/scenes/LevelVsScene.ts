98b466721b741fc841195b8b389c1510e97b0465
Jonathan McKinnell <jonathan@jam3.com>
2024-06-19 14:34:46 -0400

    fix: mute main sound

--- packages/frontend/src/game/scenes/LevelVsScene.ts
+++ packages/frontend/src/game/scenes/LevelVsScene.ts
@@ -19,7 +19,10 @@ export class LevelVsScene extends Scene {
     const gotoLevel = () => this.sceneManager.showLevel(this.levelNo)
 
     ;(async () => {
-      await this.playVideo(`vs_level_0${getWrappedLevelNo(this.levelNo)}`, () => gotoLevel())
+      await this.playVideo(`vs_level_0${getWrappedLevelNo(this.levelNo)}`, () => {
+        console.log('BING')
+        gotoLevel()
+      })
     })()
   }
 }
